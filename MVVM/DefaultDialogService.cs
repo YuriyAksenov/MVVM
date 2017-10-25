ab0c1d995d4e4f4c4dca329e893ae1011be344ea
Yuriy Aksenov <Yura290610@yandex.ru>
2017-10-25 18:02:29 +0300

    Добавьте файлы проекта.

---
+++ MVVM/DefaultDialogService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace MVVM
+{
+    public class DefaultDialogService : IDialogService
+    {
+        public string FilePath { get; set; }
+
+        public bool OpenFileDialog()
+        {
+            OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FilePath = openFileDialog.FileName;
+                return true;
+            }
+            return false;
+        }
+
+        public bool SaveFileDialog()
+        {
+            SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+            if(saveFileDialog.ShowDialog() == true)
+            {
+                FilePath = saveFileDialog.FileName;
+                return true;
+            }
+            return false;
+        }
+
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
---
+++ MVVM/JsonFileService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    public class JsonFileService : IFileService
+    {
+        public List<Phone> Open(string filename)
+        {
+            List<Phone> phones = new List<Phone>();
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(List<Phone>));
+
+            using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate))
+            {
+                phones = jsonFormatter.ReadObject(fs) as List<Phone>;
+            }
+
+            return phones;
+        }
+
+        public void Save(string filename, List<Phone> phoneList)
+        {
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(List<Phone>));
+            using (FileStream fs = new FileStream(filename, FileMode.Create))
+            {
+                jsonFormatter.WriteObject(fs, phoneList);
+            }
+        }
+    }
+}
